8db197ed940da53c8a4a155006149c155ee919f5
✩clvudii✩ <78642658+cluvdii@users.noreply.github.com>
2021-08-18 20:41:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -7,7 +12,10 @@ function setup(){
     poseNet.on('pose',gotPoses)
 }
 function draw(){
-
+    background("#606363");
+    fill("#000000");
+    stroke("#ffffff");
+    square(noseX, noseY, difference);
 }
 function modelLoaded(){
     console.log("PoseNet Is Loaded");
@@ -15,5 +23,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results)
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+",noseY="+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=leftWristX-rightWristX;
     }
 }
